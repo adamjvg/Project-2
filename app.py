4a0635f17a89e5b2b92d60502a2edfddaaaf2c27
Angadjeet Sanghera <angadjeet.sanghera@gmail.com>
2022-04-21 20:51:21 +0800

    app py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import numpy as np
-
+#################################################
+# Import Dependencies
+#################################################
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import os
 
 from flask import Flask, jsonify
 
@@ -11,15 +13,16 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///spotify.sqlite")
+engine = create_engine("sqlite:///data/data.sqlite")
 
-# reflect an existing database into a new model
+# Reflect database & tables
 Base = automap_base()
-# reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-Passenger = Base.classes.passenger
+# Save reference to table
+sptfy = "spotify"
+Spotify = Base.classes.sptfy
+
 
 #################################################
 # Flask Setup
@@ -30,30 +33,22 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-
-@app.route("/data")
+@app.route("/")
 def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
-    )
-
-
-@app.route("/api/v1.0/names")
-def names():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    # Query table
+    results = session.query(Spotify).all()
+    
+    # Convert into dictionary
+    spotify_dict = dict(results)
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    # JSONify spotify table
+    return jsonify(spotify_dict)
 
-    return jsonify(all_names)
 
+if __name__ == '__main__':
+    app.run(debug=True)

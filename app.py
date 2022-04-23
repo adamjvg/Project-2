76b1f938ef89c943d9bd438778a1716c28441ef7
Angadjeet Sanghera <angadjeet.sanghera@gmail.com>
2022-04-23 11:26:10 +0800

    Flask API Working
    Unique ID added to Spotify table and first JSON output working

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-import os
 
 from flask import Flask, jsonify
 
@@ -20,8 +19,7 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save reference to table
-sptfy = "spotify"
-Spotify = Base.classes.sptfy
+Spotify = Base.classes.spotify
 
 
 #################################################
@@ -38,16 +36,38 @@ def welcome():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # Query table
-    results = session.query(Spotify).all()
-    
-    # Convert into dictionary
-    spotify_dict = dict(results)
+    # Query table data
+    results = session.query(Spotify.uniqueID, Spotify.title, Spotify.artist, Spotify.topgenre, Spotify.yearreleased, Spotify.bpm, Spotify.nrgy, Spotify.dnce, Spotify.dB,
+                            Spotify.live, Spotify.val, Spotify.dur, Spotify.acous, Spotify.spch, Spotify.pop, Spotify.topyear, Spotify.artisttype).all()
 
     session.close()
 
+    # Create a dictionary from the row data and append to a list of all songs
+    spotify_table = []
+    for uniqueID, title, artist, topgenre, yearreleased, bpm, nrgy, dnce, dB, live, val, dur, acous, spch, pop, topyear, artisttype in results:
+        song_dict = {}
+        song_dict['uniqueID'] = uniqueID
+        song_dict['title'] = title
+        song_dict['artist'] = artist
+        song_dict['topgenre'] = topgenre
+        song_dict['yearreleased'] = yearreleased
+        song_dict['bpm'] = bpm
+        song_dict['nrgy'] = nrgy
+        song_dict['dnce'] = dnce
+        song_dict['dB'] = dB
+        song_dict['live'] = live
+        song_dict['val'] = val
+        song_dict['dur'] = dur
+        song_dict['acous'] = acous
+        song_dict['spch'] = spch
+        song_dict['pop'] = pop
+        song_dict['topyear'] = topyear
+        song_dict['artisttype'] = artisttype
+        spotify_table.append(song_dict)
+
+
     # JSONify spotify table
-    return jsonify(spotify_dict)
+    return jsonify(spotify_table)
 
 
 if __name__ == '__main__':
